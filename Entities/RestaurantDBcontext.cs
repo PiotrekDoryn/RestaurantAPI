22a573af5727820f890e29ebbce325637d939494
Piotr Doryń <piotrekdoryn@gmail.com>
2022-01-08 22:53:53 +0100

    12. Utworzenie bazy danych

---
+++ Entities/RestaurantDBcontext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDBcontext : DbContext
+    {
+        private string _connectionstring =
+            "Data Source=DELL-PIOTREK\\SQLEXPRESS;Database=RestaurantDb;Trusted_Connection=True;";
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionstring);
+        }
+    }
+}
---
+++ Migrations/20220108215150_FKAdress.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantAPI.Migrations
+{
+    public partial class FKAdress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Restaurants_RestaurantId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_RestaurantId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "Addresses");
+
+            migrationBuilder.RenameColumn(
+                name: "AddresId",
+                table: "Restaurants",
+                newName: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_AddressId",
+                table: "Restaurants",
+                column: "AddressId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Restaurants_Addresses_AddressId",
+                table: "Restaurants",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Restaurants_Addresses_AddressId",
+                table: "Restaurants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Restaurants_AddressId",
+                table: "Restaurants");
+
+            migrationBuilder.RenameColumn(
+                name: "AddressId",
+                table: "Restaurants",
+                newName: "AddresId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RestaurantId",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_RestaurantId",
+                table: "Addresses",
+                column: "RestaurantId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Restaurants_RestaurantId",
+                table: "Addresses",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
