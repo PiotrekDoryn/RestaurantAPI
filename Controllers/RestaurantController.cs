e405d08394222a309caffb77eac498e282ba1063
Piotr Doryń <piotrekdoryn@gmail.com>
2022-01-10 22:17:37 +0100

    15. Pobieranie zasobów

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private RestaurantDBcontext _dbContext;
+
+        public RestaurantController(RestaurantDBcontext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Return All Restaurants
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Restaurant))]
+        public ActionResult<IEnumerable<Restaurant>> GetAllRestaurants()
+        {
+            var result = _dbContext.Restaurants.ToList();
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Return All Restaurant by Id number
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Restaurant))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<IEnumerable<Restaurant>> GetRestaurantById([FromRoute] int id)
+        {
+            var result = _dbContext.Restaurants.FirstOrDefault(x => x.Id == id);
+            if(result is null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
